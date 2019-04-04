59b94d21d17153890eb4a3db238b13f4ddbb3d8c
Yakuphan Gökgöz <yakuphan7@gmail.com>
2019-04-04 22:23:10 +0300

    Update

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Giriş &lsaquo; Yakuphan Gökgöz &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='http://yakuphan7.github.io/wp-includes/css/dashicons.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='http://yakuphan7.github.io/wp-includes/css/buttons.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='http://yakuphan7.github.io/wp-admin/css/forms.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='http://yakuphan7.github.io/wp-admin/css/l10n.min.css?ver=5.1.1' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='http://yakuphan7.github.io/wp-admin/css/login.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://yakuphan7.github.io/wp-includes/css/dashicons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://yakuphan7.github.io/wp-includes/css/buttons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://yakuphan7.github.io/wp-admin/css/forms.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://yakuphan7.github.io/wp-admin/css/l10n.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://yakuphan7.github.io/wp-admin/css/login.min.css?ver=5.1.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="WordPress&#039;in desteğiyle">WordPress'in desteğiyle</a></h1>
 	
-	<form name="loginform" id="loginform" action="http://yakuphan7.github.io/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://yakuphan7.github.io/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Kullanıcı adı ya da e-posta adresi<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -34,13 +34,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Beni hatırla</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
-				<input type="hidden" name="redirect_to" value="http://yakuphan7.github.io/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://yakuphan7.github.io/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="http://yakuphan7.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+					<a href="https://yakuphan7.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="http://yakuphan7.github.io/">
+			<p id="backtoblog"><a href="https://yakuphan7.github.io/">
 		&larr; Yakuphan Gökgöz sitesine geri dön	</a></p>
 			
 	</div>
