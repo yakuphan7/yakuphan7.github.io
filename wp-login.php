8725e958bbfd11c4881f46271943dee5b60751d9
Yakuphan Gökgöz <yakuphan7@gmail.com>
2019-09-28 16:55:43 +0300

    Newly site

---
+++ wp-login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="tr">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="tr">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Giriş &lsaquo; Yakuphan Gökgöz &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/dashicons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/buttons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/forms.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/l10n.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/login.min.css?ver=5.2.3' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" sizes="32x32" />
+<link rel="icon" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" />
+<meta name="msapplication-TileImage" content="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-tr-tr">
+		<div id="login">
+		<h1><a href="https://wordpress.org/">WordPress'in desteğiyle</a></h1>
+	
+	<form name="loginform" id="loginform" action="https://yakuphangokgoz.github.io/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Kullanıcı adı ya da e-posta adresi<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Parola<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Beni hatırla</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
+				<input type="hidden" name="redirect_to" value="https://yakuphangokgoz.github.io/wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+					<a href="https://yakuphangokgoz.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="https://yakuphangokgoz.github.io/">
+		&larr; Yakuphan Gökgöz sitesine geri dön	</a></p>
+			
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
