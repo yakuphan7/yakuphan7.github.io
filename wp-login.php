4fc78400620b333c62371f662d00fe6d8a965d95
Yakuphan Gökgöz <yakuphan7@gmail.com>
2021-05-23 13:18:08 +0300

    Experience update

--- wp-login.php
+++ wp-login.php
@@ -1,73 +1,113 @@
 <!DOCTYPE html>
-	<!--[if IE 8]>
-		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="tr">
-	<![endif]-->
-	<!--[if !(IE 8) ]><!-->
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="tr">
-	<!--<![endif]-->
+	<html lang="tr">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Giriş &lsaquo; Yakuphan Gökgöz &#8212; WordPress</title>
-	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/dashicons.min.css?ver=5.2.10' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/buttons.min.css?ver=5.2.10' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/forms.min.css?ver=5.2.10' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/l10n.min.css?ver=5.2.10' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/login.min.css?ver=5.2.10' type='text/css' media='all' />
-	<meta name='robots' content='noindex,noarchive' />
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/dashicons.min.css?ver=5.7.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://yakuphangokgoz.github.io/wp-includes/css/buttons.min.css?ver=5.7.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/forms.min.css?ver=5.7.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/l10n.min.css?ver=5.7.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://yakuphangokgoz.github.io/wp-admin/css/login.min.css?ver=5.7.2' type='text/css' media='all' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
 	<link rel="icon" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" sizes="32x32" />
 <link rel="icon" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" />
+<link rel="apple-touch-icon" href="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" />
 <meta name="msapplication-TileImage" content="https://yakuphangokgoz.github.io/wp-content/uploads/2019/09/site_icon4.png" />
 	</head>
-	<body class="login login-action-login wp-core-ui  locale-tr-tr">
+	<body class="login no-js login-action-login wp-core-ui  locale-tr-tr">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
 		<div id="login">
 		<h1><a href="https://wordpress.org/">WordPress'in desteğiyle</a></h1>
 	
-	<form name="loginform" id="loginform" action="https://yakuphangokgoz.github.io/wp-login.php" method="post">
-	<p>
-		<label for="user_login">Kullanıcı adı ya da e-posta adresi<br />
-		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
-	</p>
-	<p>
-		<label for="user_pass">Parola<br />
-		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
-	</p>
-			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Beni hatırla</label></p>
-	<p class="submit">
-		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
-				<input type="hidden" name="redirect_to" value="https://yakuphangokgoz.github.io/wp-admin/" />
-					<input type="hidden" name="testcookie" value="1" />
-	</p>
-	</form>
+		<form name="loginform" id="loginform" action="https://yakuphangokgoz.github.io/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Kullanıcı adı ya da e-posta adresi</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
 
-			<p id="nav">
-					<a href="https://yakuphangokgoz.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
-				</p>
-	
-	<script type="text/javascript">
-	function wp_attempt_focus(){
-	setTimeout( function(){ try{
-			d = document.getElementById('user_login');
-				d.focus();
-	d.select();
-	} catch(e){}
-	}, 200);
-	}
+			<div class="user-pass-wrap">
+				<label for="user_pass">Parola</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Parolayı göster">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Beni hatırla</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Giriş" />
+									<input type="hidden" name="redirect_to" value="https://yakuphangokgoz.github.io/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
 
-			wp_attempt_focus();
-			if(typeof wpOnload=='function')wpOnload();
-			</script>
-
-			<p id="backtoblog"><a href="https://yakuphangokgoz.github.io/">
-		&larr; Yakuphan Gökgöz sitesine geri dön	</a></p>
-			
-	</div>
-
-	
-		<div class="clear"></div>
+					<p id="nav">
+								<a href="https://yakuphangokgoz.github.io/wp-login.php?action=lostpassword">Parolanızı mı unuttunuz?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="https://yakuphangokgoz.github.io/">&larr; Yakuphan Gökgöz sitesine geri dön</a>		</p>
+			</div>
+	<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+<script type='text/javascript' id='zxcvbn-async-js-extra'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/yakuphangokgoz.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+<script type='text/javascript' id='wp-polyfill-js-after'>
+( 'fetch' in window ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="https://yakuphangokgoz.github.io/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"></scr' + 'ipt>' );
+</script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1' id='wp-hooks-js'></script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67' id='wp-i18n-js'></script>
+<script type='text/javascript' id='wp-i18n-js-after'>
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script type='text/javascript' id='password-strength-meter-js-extra'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Parola g\u00fcc\u00fc bilinmiyor","short":"\u00c7ok zay\u0131f","bad":"Zay\u0131f","good":"Orta","strong":"G\u00fc\u00e7l\u00fc","mismatch":"Parolalar uyu\u015fmuyor"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='password-strength-meter-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2021-04-15 03:34:29+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"tr"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%2$s s\u00fcr\u00fcm\u00fcnden bu yana %1$s devre d\u0131\u015f\u0131 b\u0131rak\u0131lm\u0131\u015ft\u0131r! Bunun yerine %3$s kullan\u0131n. L\u00fctfen daha kapsaml\u0131 kod yazmay\u0131 g\u00f6z \u00f6n\u00fcnde bulundurun."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-admin/js/password-strength-meter.min.js?ver=5.7.2' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
+<script type='text/javascript' id='wp-util-js-extra'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/yakuphanbio\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-includes/js/wp-util.min.js?ver=5.7.2' id='wp-util-js'></script>
+<script type='text/javascript' id='user-profile-js-extra'>
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"261659a636"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='user-profile-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2021-04-15 03:34:29+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"tr"},"Your new password has not been saved.":["Yeni parolan\u0131z kaydedilemedi."],"Hide":["Gizle"],"Show":["G\u00f6ster"],"Confirm use of weak password":["Zay\u0131f parola kullanmay\u0131 onayla"],"Hide password":["Parolay\u0131 gizle"],"Show password":["Parolay\u0131 g\u00f6ster"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script type='text/javascript' src='https://yakuphangokgoz.github.io/wp-admin/js/user-profile.min.js?ver=5.7.2' id='user-profile-js'></script>
+	<div class="clear"></div>
 	</body>
 	</html>
 	
